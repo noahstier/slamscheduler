6030105c9ddc4ab8c71d2da4d17949dc769f0eca
Noah Stier <noah.stier@gmail.com>
2016-08-20 08:42:05 -0700

    add dependency erlang-algorithms

--- mix.exs
+++ mix.exs
@@ -37,7 +37,9 @@ defmodule Slamscheduler.Mixfile do
      {:phoenix_html, "~> 2.6"},
      {:phoenix_live_reload, "~> 1.0", only: :dev},
      {:gettext, "~> 0.11"},
-     {:cowboy, "~> 1.0"}]
+     {:cowboy, "~> 1.0"},
+     {:erlang_algorithms, github: 'aggelgian/erlang-algorithms', app: false}
+   ]
   end
 
   # Aliases are shortcuts or tasks specific to the current project.
